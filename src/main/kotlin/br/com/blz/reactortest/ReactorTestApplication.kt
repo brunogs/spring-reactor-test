a83679965a005ce9e2d6bfe45877c2c5d4e38c3d
Bruno Silva <brunogs.sp@gmail.com>
2018-07-22 14:37:41 -0300

    add: project generated by spring

---
+++ src/main/kotlin/br/com/blz/reactortest/ReactorTestApplication.kt
@@ -0,0 +1,11 @@
+package br.com.blz.reactortest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactorTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReactorTestApplication>(*args)
+}
